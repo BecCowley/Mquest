bdd59649de0813112d5d3a4cf92c1add094d0ccf
Bec <rebecca.cowley@csiro.au>
2022-05-17 14:52:40 +1000

    updates to fix batch and serial information

--- Util/fix_batch_serial_info.m
+++ Util/fix_batch_serial_info.m
@@ -8,7 +8,7 @@ stnnum = str2num(ncread([prefix '_keys.nc'],'stn_num')');
 
 %%
 %serial numbers and batch dates:
-serial = NaN*ones(length(stnnum),1); batch = serial;
+serial = NaN*ones(length(stnnum),1); batch = serial;hh = serial;
 for aa=1:length(stnnum)
     
     raw= 0;
@@ -31,24 +31,41 @@ for aa=1:length(stnnum)
             serial(aa) = NaN;
         end            
     end
+    kk=strmatch('HTL$',srfccodes');
+    if(~isempty(kk))
+        hh(aa) = str2num(srfcparm(:,kk)');
+    end
     
 end
 
-%% plot
-plot(batch,serial,'ko')
+% plot
+% ii = find(contains(crid,'LA2107A'));
+ii = 1:length(stnnum);
+figure(1);clf
+plot(batch(ii),serial(ii),'ko')
+datetick('x','mm/yy')
+grid
+title(prefix)
+figure(2);clf
+plot(hh(ii))
+title('Launch Height')
 
 %% 20xx dates - which cruise?
 %adjust this to suit each fix
 
 % ii = find(batch > datenum('01/02/2011','dd/mm/yyyy') & batch < datenum('01/01/2012','dd/mm/yyyy'));%serial > 1000000);% & serial < 1285000)% & b% 
-ii = find(stnnum == 89013822);
-stnnum(ii)
+% ii = find(stnnum == 89013822);
+% stnnum(ii)
+% ii = 1:length(stnnum);
+ij = find(serial(ii) > 1310000);
 for a = 1:length(ii)
     disp(stnnum(ii(a)))
     disp(serial(ii(a)))
     disp(datestr(batch(ii(a))));
+    disp(hh(ii(a)))
     nb = datestr(batch(ii(a)));
     ns = num2str(serial(ii(a)));
+    nh = num2str(hh(ii(a)));
     nb = input(['Enter new batch date [' nb '],yyyymmdd:'],'s');
     if isempty(nb)
         nb = datestr(batch(ii(a)),'yyyymmdd');
@@ -58,12 +75,18 @@ for a = 1:length(ii)
         ns = num2str(serial(ii(a)));
     end
     
+    nh = input(['Enter new launch height [' nh ']:'],'s');
+    if isempty(nh)
+        nh = num2str(hh(ii(a)));
+    end
+    
     batch(ii(a)) = datenum(nb,'yyyymmdd');
     serial(ii(a)) = str2num(ns);
+    hh(ii(a)) = str2num(nh);
 end
 %%
 %write back to file:
-for aa=ii:length(stnnum)
+for aa=1:length(stnnum)
     for bb = 1:2;
         raw= bb -1;
         filen=getfilename(num2str(stnnum(aa)),raw);
@@ -82,6 +105,12 @@ for aa=ii:length(stnnum)
             ser = num2str(serial(aa))';
             srfcparm(:,kk) = '          ';
             srfcparm(1:length(ser),kk)=ser;
+        end
+        kk=strmatch('HTL$',srfccodes');
+        if(~isempty(kk))
+            heig = num2str(hh(aa))';
+            srfcparm(:,kk) = '          ';
+            srfcparm(1:length(heig),kk)=heig;
         end
         ncwrite(filenam,'SRFC_Code',srfccodes)
         ncwrite(filenam,'SRFC_Parm',srfcparm)
